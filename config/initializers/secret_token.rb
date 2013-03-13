4282c88ff59b9a247efaf56b3306f6308e4d7116
Lene Krog <lm_krog@hotmail.com>
2013-03-13 22:17:20 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hundeliv::Application.config.secret_token = '632b0dd23a65a9847ece0c0bdd646d8f4e0ba273cf66ec775852cac5fc1e1ddafec29e5e3ef10d4ad1e93765336aabf84bc8d8fc1e649c9514e7a5694460110c'
